3a380b205b259af28970d45cbf0c18bce7b618c9
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-08-24 10:16:17 +0300

    remove redundant load path addition

--- main.rb
+++ main.rb
@@ -1,6 +1,3 @@
-simulator = File.expand_path('simulator', __dir__)
-$LOAD_PATH.unshift(simulator) unless $LOAD_PATH.include?(simulator)
-
 require_relative 'simulator/app'
 
 app = Simulator::App.new
